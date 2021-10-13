5d4ff9eb5bf9d16a1037048884b8132f1ad89a1e
IvanHansmann <carlos.hansmann19@gmail.com>
2021-10-13 00:01:31 -0500

    Ivan

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const routerApi = require('./routes')
-const express = require ('express')
+const express = require('express')
 const app = express()
-const port = 3000
+const { config } = require('./config/index')
 
-app.get('/',(req, res ) =>{
-  res.send('¡Hola bienvenidos a FanStore!')
+const port = config.port
+
+const mongoose = require ('mongoose')
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const DB_NAME = encodeURIComponent(config.dbName)
+const MONGO_URI =  `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`
+
+console.log ('***')
+console.log(MONGO_URI)
+mongoose.connect(MONGO_URI,
+  {useNewUrlParser: true, useUnifiedTopology: true}
+  ).then (()=> console.log('La conexion a sido exitosa')).catch(e=>console.log(e))
+
+app.get('/', (req, res)=>{
+    res.send('Hi body! Welcome to node and express js')
 })
+
 
 routerApi(app)
 
-app.listen(port, () =>{
-  console.log(`Listening at http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Listening at http://localhost/:${port}`)
 })
-
